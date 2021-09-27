e0ff11b3026c2f86f20a0cf569c6e3fa5bddde92
Marlon SC <64659366+marlonedusc@users.noreply.github.com>
2021-09-27 13:46:04 -0500

    Update eval.php

--- eval.php
+++ eval.php
@@ -1,4 +1,26 @@
 <?php
-$numero = rand(0, 100);
-echo $numero;
+$canal = $_GET["opcion"];
+$instrucciones = 'MODO DE USO: !canal (opción). Usa "!canal lista" para ver la lista de opciones disponibles.';
+if(isset($canal) && $canal != ""){
+   $prefijo = "https://www.twitch.tv/";
+   $canales = array(
+      "alexuz" => $prefijo."ialexuz/",
+      "godz" => $prefijo."godzito/",
+      "smoxx" => $prefijo."smoxxdesigns/",
+      "tecno" => $prefijo."tecnomercy/",
+      "camila" => $prefijo."camilasatv/"
+   );
+
+   if(array_key_exists($canal, $canales)){
+	echo $canales[$canal];
+   }else if($canal == "lista"){
+	foreach ($canales as $un_canal => $valor){
+		echo $un_canal." | ";
+      }
+   }else{
+	echo $instrucciones;
+   }
+}else{
+   echo $instrucciones;
+}
 ?>
